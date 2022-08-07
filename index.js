ddcfa241f367fb8660a8ca341a0da90e6217ea8b
anurag1905-hub <anuragharsh626@gmail.com>
2022-08-07 16:23:11 +0530

    User SignIn and SignUp

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const db = require('./config/mongoose');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 
 require('dotenv').config();
 
-app.use('/',require('./routes/index'));
-
 const port = process.env.port || 8000;
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(expressValidator());
+
+app.use('/',require('./routes/index'));
 
 app.listen(port,function(err){
     if(err){
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/usersController');
+const validateData = require('../validator');
 
-router.get('/',usersController.users);
+router.post('/signup',validateData.userSignupValidator,usersController.signup);
+router.post('/signin',usersController.signin);
+router.get('/signout',usersController.signout);
 
 module.exports = router;
