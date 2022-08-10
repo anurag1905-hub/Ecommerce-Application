b257dc6808a1889bf33bf78bd853a864f8966a96
anurag1905-hub <anuragharsh626@gmail.com>
2022-08-10 20:19:22 +0530

    Add delete,update and list all feature to categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,17 +1,83 @@
 const Category = require('../models/category');
 const handleError = require('../helpers/dbErrorHandler');
 
-module.exports.create = function(req,res){
-    Category.create(req.body,function(err,data){
-        if(err){
+module.exports.categoryById = function (req, res, next, id) {
+    Category.findById(id, function (err, category) {
+        if (err||!category) {
+            return res.status(400).json({
+                error: 'Either there was an error or the category does not exist'
+            });
+        }
+        else{
+            req.category=category;
+        }
+        next();
+    });
+}
+
+module.exports.read = function(req,res){
+    return res.json(
+        req.category
+    );
+}
+
+module.exports.create = function (req, res) {
+    Category.create(req.body, function (err, data) {
+        if (err) {
             console.log(req.body);
             console.log(err);
+            return res.status(400).json({
+                error: handleError.errorHandler(err)
+            });
+        }
+        else {
+            return res.json({ data });
+        }
+    });
+}
+
+module.exports.update = function(req,res){
+    const category = req.category;
+    console.log(category);
+    category.name = req.body.name;
+    console.log(category.name);
+    category.save(function(err,data){
+        if(err){
             return res.status(400).json({
                 error:handleError.errorHandler(err)
             });
         }
         else{
-            return res.json({data});
+            return res.json(data);
+        }
+    });
+}
+
+module.exports.remove = function(req,res){
+    const category = req.category;
+    category.remove(function(err){
+        if(err){
+            return res.status(400).json({
+                error:handleError.errorHandler(err)
+            });
+        }
+        else{
+            return res.json({
+                message:"Category deleted successfully."
+            });
+        }
+    });
+}
+
+module.exports.list = function(req,res){
+    Category.find({},function(err,data){
+        if(err){
+            return res.status(400).json({
+                error:handleError.errorHandler(err)
+            });
+        }
+        else {
+            return res.json(data);
         }
     });
 }
--- routes/category.js
+++ routes/category.js
@@ -6,7 +6,13 @@ const authController = require('../controllers/authController');
 const usersController = require('../controllers/usersController');
 
 router.post("/create/:userId",authController.requireSignIn,authController.isAuth,authController.isAdmin,categoryController.create);
+router.put('/update/:userId/:categoryId',authController.requireSignIn,authController.isAuth,authController.isAdmin,categoryController.update);
+router.delete("/delete/:userId/:categoryId",authController.requireSignIn,authController.isAuth,authController.isAdmin,categoryController.remove);
+router.get('/all',categoryController.list);
+router.get('/:categoryId',categoryController.read); // Put this at last due to router.param otherwise '/all' route doesn't work.
+
 
 router.param("userId",usersController.userById);
+router.param("categoryId",categoryController.categoryById);
 
 module.exports = router;
