5d8b119098c56ae06139401530191afdf10b1246
anurag1905-hub <anuragharsh626@gmail.com>
2022-08-10 19:17:14 +0530

    Add update product functionality

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -4,9 +4,22 @@ const Product = require('../models/product');
 const fs = require('fs');
 const handleError = require('../helpers/dbErrorHandler');
 
+module.exports.productById = function(req,res,next,id){
+    Product.findById(id,function(err,product){
+        if(err||!product){
+            return res.status(400).json({
+                error:"Product not found"
+            });
+        }
+        else{
+            req.product = product;
+        }
+        next();
+    });
+}
 
 module.exports.create = function(req,res){
-    const form = formidable({keepExtensions:false});
+    const form = formidable({keepExtensions:true});
     form.parse(req,function(err,fields,files){
         if(err){
             console.log(Error);
@@ -23,6 +36,73 @@ module.exports.create = function(req,res){
                 });
             }
             let product = new Product(fields);
+            if(files.photo){
+                if(files.photo.size>1000000){
+                    return res.status(400).json({
+                        error:"Image should be less than 1mb in size"
+                    });
+                }
+                else{
+                    product.photo.data = fs.readFileSync(files.photo.filepath);
+                    product.photo.contentType = files.photo.mimetype;
+                }
+            }
+            product.save(function(err,result){
+                if(err){
+                    console.log('Not able to save');
+                    return res.status(400).json({
+                        error:handleError.errorHandler(err)
+                    });
+                }
+                else{
+                    return res.json(result);
+                }
+            });
+        }
+    });
+}
+
+module.exports.read = function(req,res){
+    req.product.photo = undefined; //Dont send image bcz it is too large.
+    return res.json(req.product);
+}
+
+module.exports.remove = function(req,res){
+    let product = req.product;
+    product.remove(function(err){
+        if(err){
+            return res.status(400).json({
+                error:handleError.errorHandler(err)
+            });
+        }
+        else{
+            return res.json({
+                message:"Product deleted successfully"
+            });
+        }
+    });
+}
+
+module.exports.update = function(req,res){
+    const form = formidable({keepExtensions:true});
+    form.parse(req,function(err,fields,files){
+        if(err){
+            console.log(Error);
+            return res.status(400).json({
+                error:"Image could not be uploaded"
+            });
+        }
+        else{
+            // check for all fields.
+            const {name,description,price,category,quantity,shipping} = fields;
+            if(!name||!description||!price||!category||!quantity||!shipping){
+                return res.status(400).json({
+                    error:"All fields are required"
+                });
+            }
+            let product = req.product;
+            // update the product
+            product = _.extend(product,fields);
             if(files.photo){
                 if(files.photo.size>1000000){
                     return res.status(400).json({
--- routes/product.js
+++ routes/product.js
@@ -7,7 +7,11 @@ const usersController = require('../controllers/usersController');
 const productController = require('../controllers/productsController');
 
 router.post("/create/:userId",authController.requireSignIn,authController.isAuth,authController.isAdmin,productController.create);
+router.get('/read/:productId',productController.read);
+router.delete('/delete/:userId/:productId',authController.requireSignIn,authController.isAuth,authController.isAdmin,productController.remove);
+router.put('/update/:userId/:productId',authController.requireSignIn,authController.isAuth,authController.isAdmin,productController.update);
 
 router.param("userId",usersController.userById);
+router.param("productId",productController.productById);
 
 module.exports = router;
